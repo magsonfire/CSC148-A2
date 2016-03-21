64e899b5307da983e0cd6a3f3898445a875847cc
kaitlynli64 <kaitlyn.linda.li@hotmail.com>
2016-03-21 00:40:22 -0400

    I added more to extensions! almost done :D

--- grid_peg_solitaire_puzzle.py
+++ grid_peg_solitaire_puzzle.py
@@ -31,30 +31,80 @@ class GridPegSolitairePuzzle(Puzzle):
     def extensions(self):
     # legal extensions consist of all configurations that can be reached by
     # making a single jump from this configuration
+
         #helper function to find Pegs
-        def find_peg(row):
-            for row in self._marker:
-                for x in row:
-                    star_index = []
-                    if x.value == '*':
-                        star_index = x.index
+        def find_peg(marker):
+            peg_locations = []
+            for x in range(len(marker)):
+                for y in range(len(marker[x])):
+                    if marker[x][y] == '*':
+                        peg_locations.append((x,y))
+
+            return peg_locations
 
         def get_position(x,y):
             #check if X and Y are in the proper bounds
-            if x > 0 and x <= len(self._marker) and y > 0 and y <= len(self._marker[0]):
-                return self._marker[x-1][y-1]
+            if x >= 0 and x < len(self._marker) and y >= 0 and y < len(self._marker[0]):
+                return self._marker[x][y]
             else:
                 return None
 
         def check_right(x,y):
-            return get_position(x+1,y) == "*" and get_position(x+2,y) == '.'
+            return get_position(x,y+1) == "*" and get_position(x,y+2) == '.'
 
         def move_right(x,y):
             new_config = self._marker[:]
-            new_config[x] = '.'
-            new_config[x+1] = '.'
-            new_config[x+2] = '*'
+            new_config[x][y] = '.'
+            new_config[x][y+1] = '.'
+            new_config[x][y+2] = '*'
             return GridPegSolitairePuzzle(new_config, self._marker_set)
+
+        def check_left(x,y):
+            return get_position(x,y-1) == "*" and get_position(x,y-2) =='.'
+
+        def move_left(x,y):
+            new_config = self._marker[:]
+            new_config[x][y] ='.'
+            new_config[x][y-1] = '.'
+            new_config[x][y-2] = '*'
+            return GridPegSolitairePuzzle(new_config,self._marker_set)
+
+        def check_up(x,y):
+            return get_position(x-1, y) == '*' and get_position(x-2,y) == '.'
+
+        def move_up(x,y):
+            new_config = self._marker[:]
+            new_config[x][y] ='.'
+            new_config[x-1][y] = '.'
+            new_config[x-2][y] = '*'
+            return GridPegSolitairePuzzle(new_config,self._marker_set)
+
+        def check_down(x,y):
+            return get_position(x+1, y) == '*' and get_position(x+2,y) == '.'
+
+        def move_down(x,y):
+            new_config = self._marker[:]
+            new_config[x][y] ='.'
+            new_config[x+1][y] = '.'
+            new_config[x+2][y] = '*'
+            return GridPegSolitairePuzzle(new_config,self._marker_set)
+
+        possible_extensions = []
+
+        for peg in find_peg(self._marker):
+            if check_right(peg[0],peg[1]):
+                possible_extensions.append(move_right(peg[0],peg[1]))
+
+            if check_left(peg[0],peg[1]):
+                possible_extensions.append(move_left(peg[0],peg[1]))
+
+            if check_up(peg[0],peg[1]):
+                possible_extensions.append(move_up(peg[0],peg[1]))
+
+            if check_down(peg[0],peg[1]):
+                possible_extensions.append(move_down(peg[0],peg[1]))
+
+        return possible_extensions
 
     def is_solved(self):
         star_count = 0
